28d0cde841b96a2ecaaae548604d0fd45bf879a0
xiaowei wan <xwan@evernote.com>
2018-06-03 14:29:27 -0700

    Init todo and first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### Django Rest Framework & React Todo
---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    todos: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/');
+      const todos = await res.json();
+      this.setState({
+        todos
+      });
+    } catch (e) {
+      console.log(e);
+    } 
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.todos.map(item => (
+          <div>
+            <h1>{item.title}</h1>
+            <span>{item.description}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
